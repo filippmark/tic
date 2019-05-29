7989bc9c74648502680514c20247668b168127cb
filipp <lenovofm12@outlook.com>
2019-05-29 18:50:10 +0300

    Updates

---
+++ precache-manifest.4929cf531b01bb721fe5935d5ac29c88.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c139d2cb9e78b8db2db",
+    "url": "/tic/static/js/main.4c9024b0.chunk.js"
+  },
+  {
+    "revision": "758444f9dcc465a74bac",
+    "url": "/tic/static/js/runtime~main.d1380ffe.js"
+  },
+  {
+    "revision": "0bd08c5f063516b7ff94",
+    "url": "/tic/static/js/2.0c9cd3e4.chunk.js"
+  },
+  {
+    "revision": "70f57d85b0b7a62090d4263c6365d8bd",
+    "url": "/tic/index.html"
+  }
+];
